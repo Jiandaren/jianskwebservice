cd729fccb7398d9c88820cb26412405ff81b508f
jiansk <jiansk20361@hundsun.com>
2018-08-06 00:09:28 +0800

    [feat]:修改打包设置，打成war包部署
    (cherry picked from commit 43c5ac8)
    
    [fix]:版本号更新为0.0.2

---
+++ src/main/java/cn/jiansk/JianskwebserviceServletInitializer.java
@@ -0,0 +1,20 @@
+package cn.jiansk;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/*********************************************************
+ * 文件名称：JianskwebserviceServletInitializer
+ * 系统名称：交易银行系统V1.2
+ * 模块名称：cn.jiansk 
+ * 功能说明：
+ * 开发人员：jiansk20361
+ * 开发时间：2018-08-04 16:34
+ * 修改记录：程序版本    修改日期    修改人员    修改单号    修改说明
+ *********************************************************/
+public class JianskwebserviceServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JianskwebserviceApplication.class);
+    }
+}
