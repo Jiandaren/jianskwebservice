897573f00e24a42f6b3bcea2d7653b4239577f5e
jiansk <jiansk20361@hundsun.com>
2018-08-13 03:21:44 +0800

    [feat]:美化首页和登录页面

--- src/main/java/cn/jiansk/web/HomeController.java
+++ src/main/java/cn/jiansk/web/HomeController.java
@@ -1,22 +1,28 @@
 package cn.jiansk.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /*********************************************************
- * 文件名称：UserLogin
+ * 文件名称：HomeController
  * 系统名称：交易银行系统V1.2
  * 模块名称：cn.jiansk.web 
  * 功能说明：
  * 开发人员：jiansk20361
- * 开发时间：2018-07-09 0:54
+ * 开发时间：2018-08-12 18:07
  * 修改记录：程序版本    修改日期    修改人员    修改单号    修改说明
  *********************************************************/
 @Controller
-public class UserLogin {
+public class HomeController {
 
-    @RequestMapping("/index")
+    @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "/home/login";
     }
 }
